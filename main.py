4a587391b76c5411f3289023637ad96c3a4f53f0
AshTron <ashutosh10615@gmail.com>
2023-04-05 14:53:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+width, height = 1280, 720
+folderPath = "presentation"
+
+cap = cv2.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+
+pathImages = sorted(os.listdir(folderPath), key=len)
+
+imgNumber = 0
+hs, ws = 120, 213
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
+    imgCurrent = cv2.imread(pathFullImage)
+
+    hands, img = detector.findHands(img, flipType=False)
+
+    if hands:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        print(fingers)
+
+    imgSmall = cv2.resize(img, (ws, hs))
+    h, w, _ = imgCurrent.shape
+    imgCurrent[0:hs, w-ws:w] = imgSmall
+
+    cv2.imshow("Image", img)
+    cv2.imshow("Slides", imgCurrent)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+#hello
